797c1555dd6de59d2f0ec9ae265d74c8c30e1a6f
= <AYOUBMAJID71@GMAIL.COM>
2023-12-12 15:15:34 +0100

    update course  7

--- 07 - Algorithms & Problem Solving Level 3/projects/PROJECT2/project2.cpp
+++ 07 - Algorithms & Problem Solving Level 3/projects/PROJECT2/project2.cpp
@@ -35,7 +35,7 @@ struct stClient
     string phone;
     float accountBalance;
 };
-const string fileName = "project1.txt";
+const string fileName = "project2.txt";
 
 // --------------------------- print page Section  Function--------------------------------
 
