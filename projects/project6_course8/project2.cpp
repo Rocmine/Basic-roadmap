19b6903ca1adfe0366e135e639bcba4b02337c94
= <AYOUBMAJID71@GMAIL.COM>
2024-01-01 21:10:04 +0100

    add 2 projects

---
+++ projects/project6_course8/project2.cpp
@@ -0,0 +1,14 @@
+/*
+ATM Main Menu Screen
+
+[1] Quick Withdraw.
+[2] Normal Withdraw.
+[3] Deposit
+[4] Check Balance.
+[5] Logout.
+
+Choose what do you want to do? [1 to 5]?
+
+ To Do Later inchalah :  
+*/
+
